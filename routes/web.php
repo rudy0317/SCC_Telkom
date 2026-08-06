b3d79ddfb9bcf98dae048c3ccee2bd0b8000342f
Rudy <rudy2310010148@gmail.com>
2026-08-06 16:58:36 +0800

    refactor(cleanup): remove unused Laravel files and standardize on Node.js Playwright CDP Engine

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\SccController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [SccController::class, 'index']);
-Route::get('/api/odp/search', [SccController::class, 'searchOdp']);
-Route::any('/scc/proxy/{path?}', [SccController::class, 'proxyCloseTicket'])->where('path', '.*');
